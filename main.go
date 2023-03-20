a785c89d25118892b50aa0f8716483a89874a48f
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-03-20 15:52:49 +0100

    tried to make it work

--- main.go
+++ main.go
@@ -11,15 +11,13 @@ import (
 	"github.com/go-co-op/gocron"
 )
 
-// TODO: Implement teams handler
-
 func main() {
 	utils.LoadEnvs()
 	fmt.Println("Started the Cron Scheduler for magasin advicer")
 	fmt.Println(time.Now().UTC().Format("2006-01-02 15:04:05"))
 
 	s := gocron.NewScheduler(time.UTC)
-	_, _ = s.Cron("0 4 1-5 * *").SingletonMode().Do(func() {
+	_, _ = s.Cron("0 4 * * *").SingletonMode().Do(func() {
 		fmt.Printf("%v Started the Script \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 
 		err := utils.HandleCreateAdvice()
@@ -30,5 +28,7 @@ func main() {
 		sap_api_wrapper.SapApiPostLogout()
 		fmt.Printf("%v Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 	})
+
 	s.StartBlocking()
+
 }

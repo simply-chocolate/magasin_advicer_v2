5df1a308c4166edd7549d8ec26253102ef3bac81
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-05-17 15:44:07 +0200

    added a run before starting the scheduler

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ func main() {
 	utils.LoadEnvs()
 	fmt.Println("Started the Cron Scheduler for magasin advicer")
 	fmt.Println(time.Now().UTC().Format("2006-01-02 15:04:05"))
+	fmt.Printf("%v Started the Script \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	err := utils.HandleCreateAdvice()
+	if err != nil {
+		teams_notifier.SendUnknownErrorToTeams(err)
+	}
 
 	s := gocron.NewScheduler(time.UTC)
 	_, _ = s.Cron("0 4 * * *").SingletonMode().Do(func() {

28b872c0f3a99c3fdcb291928ea35f016f4dcd29
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-11-20 12:51:23 +0100

    removed cron scheduler from main

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"magasin_advicer/utils"
 
 	"time"
-
-	"github.com/go-co-op/gocron"
 )
 
 func main() {
@@ -28,28 +26,5 @@ func main() {
 		teams_notifier.SendUnknownErrorToTeams(err)
 	}
 	fmt.Printf("%v: Success handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-
-	fmt.Printf("%v: Started the Cron Scheduler", time.Now().UTC().Format("2006-01-02 15:04:05"))
-
-	s := gocron.NewScheduler(time.UTC)
-	_, _ = s.Cron("0 * * * *").SingletonMode().Do(func() {
-
-		fmt.Printf("%v: Handling Stock Transfers \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-		err = utils.HandleCreateAdviceStockTransfers()
-		if err != nil {
-			teams_notifier.SendUnknownErrorToTeams(err)
-		}
-		fmt.Printf("%v: Success handling Stock Transfers \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-
-		fmt.Printf("%v: Handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-		err = utils.HandleCreateAdviceOrders()
-		if err != nil {
-			teams_notifier.SendUnknownErrorToTeams(err)
-		}
-		fmt.Printf("%v: Success handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-
-	})
-
-	s.StartBlocking()
 
 }

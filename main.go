1e33a3c7cdbe8a9428f1658886eb54cc881e78cc
Jedikrigeren <hasselsteen95@live.dk>
2025-02-19 15:18:50 +0100

    no longer handles advice for orders

--- main.go
+++ main.go
@@ -34,10 +34,10 @@ func main() {
 	//fmt.Printf("%v: Success handling Stock Transfers \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 	//fmt.Printf("%v: Handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 
-	err = utils.HandleCreateAdviceOrders(businessPartners, CardCodesString, validItems)
-	if err != nil {
-		teams_notifier.SendUnknownErrorToTeams(err)
-	}
+	// err = utils.HandleCreateAdviceOrders(businessPartners, CardCodesString, validItems)
+	// if err != nil {
+	// 	teams_notifier.SendUnknownErrorToTeams(err)
+	// }
 
 	//fmt.Printf("%v: Success handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 	fmt.Printf("%v: Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))

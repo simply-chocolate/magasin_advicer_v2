5a0fc565052b4e194e4b56403d409bd8d2876920
Jedikrigeren <hasselsteen95@live.dk>
2025-02-25 08:45:36 +0100

    fixed

--- main.go
+++ main.go
@@ -31,15 +31,15 @@ func main() {
 		teams_notifier.SendUnknownErrorToTeams(err)
 	}
 
-	//fmt.Printf("%v: Success handling Stock Transfers \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
-	//fmt.Printf("%v: Handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	fmt.Printf("%v: Success handling Stock Transfers \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	fmt.Printf("%v: Handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	// sad
+	err = utils.HandleCreateAdviceOrders(businessPartners, CardCodesString, validItems)
+	if err != nil {
+		teams_notifier.SendUnknownErrorToTeams(err)
+	}
 
-	// err = utils.HandleCreateAdviceOrders(businessPartners, CardCodesString, validItems)
-	// if err != nil {
-	// 	teams_notifier.SendUnknownErrorToTeams(err)
-	// }
-
-	//fmt.Printf("%v: Success handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
+	fmt.Printf("%v: Success handling Orders \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 	fmt.Printf("%v: Success \n", time.Now().UTC().Format("2006-01-02 15:04:05"))
 
 }
